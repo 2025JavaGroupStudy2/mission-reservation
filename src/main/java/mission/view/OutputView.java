d8fe93be029726b9c58cd8b2d98a8bacc90298ca
[echo0528] <[echo528@soongsil.ac.kr]>
2025-07-23 23:18:55 +0900

    [조은영] 출력표형식 23:18

--- src/main/java/mission/view/OutputView.java
+++ src/main/java/mission/view/OutputView.java
@@ -11,8 +11,8 @@ import java.util.List;
 
         public void printReservationList(List<ReservationOutputDTO> list) {
             System.out.println("\n예약 목록");
-            System.out.println("|예약 시간\t    \t\t|시작 시간\t\t    \t|종료 시간\t    \t\t|예약자 명\t|");
-            System.out.println("|-----------------|-----------------|-----------------|---------|");
+            System.out.println("|예약 시간\t\t\t\t|시작 시간\t\t\t\t|종료 시간\t\t\t\t|예약자 명\t\t\t|");
+            System.out.println("|-----------------------|-----------------------|-----------------------|--------------------|");
 
             int idx = 1;
             for (ReservationOutputDTO reservationOutputDTO : list) {
